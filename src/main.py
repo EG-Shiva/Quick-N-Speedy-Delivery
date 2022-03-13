0c42812454ecb9c4c0c684be4d6150f91923dccf
Sumanth <93238666+Sumanth1680@users.noreply.github.com>
2022-03-13 20:26:34 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
-..
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+sns.set()
+
+sns.set()
+
+data = pd.read_csv('G:/PPT/2 - 2/DAA/Project/Dataset/Dataset1.csv')
+plt.scatter(data['Longitude'], data['Latitude'])
+plt.xlim(-180, 180)
+plt.ylim(-90, 90)
+plt.show()
