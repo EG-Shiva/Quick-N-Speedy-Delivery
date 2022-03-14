d0942b736b8351b41e220bec2226ed1310d73b01
Sumanth <93238666+Sumanth1680@users.noreply.github.com>
2022-03-14 09:53:39 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,36 @@
-import pandas as pd
-import seaborn as sns
+import math
+import random
 from matplotlib import pyplot as plt
 
-sns.set()
 
-data = pd.read_csv('G:/PPT/2 - 2/DAA/Project/Dataset/Dataset1.csv')
-plt.scatter(data['Longitude'], data['Latitude'])
-plt.xlim(-180, 180)
-plt.ylim(-90, 90)
-plt.show()
+class SolveTSPUsingACO:
+    class Edge:
+        def __init__(self, a, b, weight, initial_pheromone):
+            self.a = a
+            self.b = b
+            self.weight = weight
+            self.pheromone = initial_pheromone
+
+    class Ant:
+        def __init__(self, alpha, beta, num_nodes, edges):
+            self.alpha = alpha
+            self.beta = beta
+            self.num_nodes = num_nodes
+            self.edges = edges
+            self.tour = None
+            self.distance = 0.0
+
+        def find_tour(self):
+            self.tour = [random.randint(0, self.num_nodes - 1)]
+            while len(self.tour) < self.num_nodes:
+                self.tour.append(self._select_node())
+            return self.tour
+
+        def get_distance(self):
+            self.distance = 0.0
+            for i in range(self.num_nodes):
+                self.distance += self.edges[self.tour[i]][self.tour[(i + 1) % self.num_nodes]].weight
+            return self.distance
+
+        def _select_node(self):
+            pass
